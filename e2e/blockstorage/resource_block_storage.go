fd6c2daa69e3ed66772fc774c71c1df8d8fc3d13
Sajal Pathak <sajal.pathak@e2enetworks.com>
2024-04-15 16:29:15 +0530

    terraform changes done

--- e2e/blockstorage/resource_block_storage.go
+++ e2e/blockstorage/resource_block_storage.go
@@ -235,7 +235,11 @@ func resourceUpdateBlockStorage(ctx context.Context, d *schema.ResourceData, m i
 
 	}
 
-	if d.HasChange("size") || d.HasChange("name") {
+	if d.HasChange("name") {
+		return diag.Errorf("You cannot change the name of a blockstorage resource after creation.")
+	}
+
+	if d.HasChange("size") {
 		prevName, currName := d.GetChange("name")
 		prevSize, currSize := d.GetChange("size")
 		err := validateSize(d, m)
@@ -273,11 +277,11 @@ func resourceUpdateBlockStorage(ctx context.Context, d *schema.ResourceData, m i
 			}
 			d.Set("size", prevSize)
 			d.Set("name", prevName)
-			return diag.Errorf("You cannot change the block storage size and name unless you are upgrading it")
+			return diag.Errorf("You cannot change the block storage size unless you are upgrading it")
 		} else {
 			d.Set("size", prevSize)
 			d.Set("name", prevName)
-			return diag.Errorf("You cannot upgrade a block storage name or size unless it is attached to a node")
+			return diag.Errorf("You cannot upgrade a block storage size unless it is attached to a node")
 		}
 	}
 	return resourceReadBlockStorage(ctx, d, m)
